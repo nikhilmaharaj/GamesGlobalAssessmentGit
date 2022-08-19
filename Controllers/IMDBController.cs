475d836037ca2a6beb38eae4c2cb78ed5b18e335
Nikhil <nikhil@charismatech.co.za>
2022-08-19 20:17:21 +0200

    IMDB List page and integration

---
+++ Controllers/IMDBController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GamesGlobalAssessment.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using GamesGlobalAssessment.Data;
+using Microsoft.AspNetCore.Http;
+
+namespace GamesGlobalAssessment.Controllers
+{
+    [Authorize]
+    public class IMDBController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public IMDBController(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
